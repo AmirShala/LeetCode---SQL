99af7cc8d3dc2b58532bfbe2bd463182cb630dbe
Amir <96538080+AmirShala@users.noreply.github.com>
2026-03-08 14:27:14 +0200

    Update LeetCode - SQL Training.sql

--- LeetCode - SQL Training.sql
+++ LeetCode - SQL Training.sql
@@ -1248,8 +1248,188 @@ WHERE
 
 =================================================================================================================================
 
+Q31: Product Price at a Given Date
+
+Table: Products
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| new_price     | int     |
+| change_date   | date    |
++---------------+---------+
+(product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+Each row of this table indicates that the price of some product was changed to a new price at some date.
+Initially, all products have price 10.
+
+Write a solution to find the prices of all products on the date 2019-08-16.
+
+Return the result table in any order.
+
+Solution:
+
+WITH LatestDate AS (
+    SELECT *
+    FROM (
+        SELECT 
+            Product_ID,
+            New_Price AS Price,
+            Change_Date,
+            ROW_NUMBER() OVER (
+                PARTITION BY Product_ID 
+                ORDER BY Change_Date DESC
+            ) AS RN
+        FROM 
+            Products
+        WHERE 
+            Change_Date <= '2019-08-16'
+    ) T
+    WHERE 
+        T.RN = 1
+)
+
+SELECT 
+    Product_ID,
+    Price
+FROM 
+    LatestDate
+
+UNION
+
+SELECT 
+    Product_ID,
+    10 AS Price
+FROM 
+    Products
+WHERE 
+    Product_ID NOT IN (
+        SELECT Product_ID 
+        FROM LatestDate
+    )
+
 
 =================================================================================================================================
+
+Q32: Last Person to Fit in the Bus
+
+Table: Queue
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| person_id   | int     |
+| person_name | varchar |
+| weight      | int     |
+| turn        | int     |
++-------------+---------+
+person_id column contains unique values.
+This table has the information about all people waiting for a bus.
+The person_id and turn columns will contain all numbers from 1 to n, where n is the number of rows in the table.
+turn determines the order of which the people will board the bus, where turn=1 denotes the first person to board and turn=n denotes the last person to board.
+weight is the weight of the person in kilograms.
+ 
+
+There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
+
+Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
+
+Note that only one person can board the bus at any given turn.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Queue table:
++-----------+-------------+--------+------+
+| person_id | person_name | weight | turn |
++-----------+-------------+--------+------+
+| 5         | Alice       | 250    | 1    |
+| 4         | Bob         | 175    | 5    |
+| 3         | Alex        | 350    | 2    |
+| 6         | John Cena   | 400    | 3    |
+| 1         | Winston     | 500    | 6    |
+| 2         | Marie       | 200    | 4    |
++-----------+-------------+--------+------+
+Output: 
++-------------+
+| person_name |
++-------------+
+| John Cena   |
++-------------+
+Explanation: The folowing table is ordered by the turn for simplicity.
++------+----+-----------+--------+--------------+
+| Turn | ID | Name      | Weight | Total Weight |
++------+----+-----------+--------+--------------+
+| 1    | 5  | Alice     | 250    | 250          |
+| 2    | 3  | Alex      | 350    | 600          |
+| 3    | 6  | John Cena | 400    | 1000         | (last person to board)
+| 4    | 2  | Marie     | 200    | 1200         | (cannot board)
+| 5    | 4  | Bob       | 175    | ___          |
+| 6    | 1  | Winston   | 500    | ___          |
++------+----+-----------+--------+--------------+
+
+
+Solution:
+
+SELECT TOP 1
+    Person_Name
+FROM (
+    SELECT 
+        Turn,
+        Person_Name,
+        SUM(Weight) OVER (ORDER BY Turn) AS Total_Weight
+    FROM 
+        Queue
+) T
+WHERE 
+    T.Total_Weight <= 1000
+ORDER BY 
+    Turn DESC
+
+=================================================================================================================================
+
+Q33: Count Salary Categories
+
+Table: Accounts
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key (column with unique values) for this table.
+Each row contains information about the monthly income for one bank account.
+ 
+
+Write a solution to calculate the number of bank accounts for each salary category. The salary categories are:
+
+"Low Salary": All the salaries strictly less than $20000.
+"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+"High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in a category, return 0.
+
+Return the result table in any order.
+
+
+
+Solution:
+
+SELECT
+    'Low Salary' AS Category,
+    COUNT(CASE WHEN Income < 20000 THEN 1 END) AS Accounts_Count
+FROM Accounts
+UNION ALL
+SELECT
+    'Average Salary' AS Category,
+    COUNT(CASE WHEN Income >= 20000 AND Income <= 50000 THEN 1 END) AS Accounts_Count
+FROM Accounts
+UNION ALL
+SELECT
+    'High Salary' AS Category,
+    COUNT(CASE WHEN Income > 50000 THEN 1 END) AS Accounts_Count
+FROM Accounts
 =================================================================================================================================
 =================================================================================================================================
 =================================================================================================================================
@@ -1258,7 +1438,7 @@ WHERE
 =================================================================================================================================
 =================================================================================================================================
 =================================================================================================================================
-=================================================================================================================================
+
 
 
 
