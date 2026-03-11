403e020bace39aeb93820d88572ff6fb50e6493f
Amir <96538080+AmirShala@users.noreply.github.com>
2026-03-11 16:44:42 +0200

    Update LeetCode - SQL Training.sql

--- LeetCode - SQL Training.sql
+++ LeetCode - SQL Training.sql
@@ -1572,11 +1572,60 @@ FROM (
 
 =================================================================================================================================
 
-Q37: 
+Q37: Restaurant Growth
+
+Table: Customer
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |
+| name          | varchar |
+| visited_on    | date    |
+| amount        | int     |
++---------------+---------+
+In SQL,(customer_id, visited_on) is the primary key for this table.
+This table contains data about customer transactions in a restaurant.
+visited_on is the date on which the customer with ID (customer_id) has visited the restaurant.
+amount is the total paid by a customer.
+ 
+
+You are the restaurant owner and you want to analyze a possible expansion (there will be at least one customer every day).
+
+Compute the moving average of how much the customer paid in a seven days window (i.e., current day + 6 days before). average_amount should be rounded to two decimal places.
+
+Return the result table ordered by visited_on in ascending order.
 
 
 Solution:
 
+WITH AggregatedAmountByDate AS (
+    SELECT
+        Visited_On,
+        SUM(Amount) AS Daily_Amount
+    FROM Customer
+    GROUP BY Visited_On
+)
+SELECT
+    Visited_On,
+    Amount,
+    Average_Amount
+FROM (
+    SELECT
+        Visited_On,
+        SUM(Daily_Amount) OVER (
+            ORDER BY Visited_On
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS Amount,
+        ROUND(
+            AVG(Daily_Amount * 1.0) OVER (
+                ORDER BY Visited_On
+                ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+            ), 2
+        ) AS Average_Amount,
+        ROW_NUMBER() OVER (ORDER BY Visited_On) AS Rn
+    FROM AggregatedAmountByDate
+) T
+WHERE T.Rn >= 7
 
 =================================================================================================================================
 
@@ -1589,6 +1638,7 @@ Solution:
 =================================================================================================================================
 =================================================================================================================================
 =================================================================================================================================
+
 
 
 
